ce5ddf70d35609299ad566105603aa64a547a592
Eduardo Vidals <eduardovidals69@gmail.com>
2021-06-12 10:17:22 -0400

    Reward function for AWS DeepRacer that improves center encouragement

--- RewardFunction.04_CenterImprovement/reward_function.py
+++ RewardFunction.04_CenterImprovement/reward_function.py
@@ -12,16 +12,16 @@ def reward_function(params):
     # encourage for all wheels to be on track
     if all_wheels_on_track:
         # initially encourage to stay in the center
-        if steps < 15:
+        if steps < 40:
             reward = (1 - (distance_from_center / (track_width / 2)) ** 4) * speed ** 2
         # after 15 steps reward the car based on track completion and speed
-        if steps >= 15:
+        if steps >= 40:
             reward = ((progress * speed ** 2) / steps) * 2
     else:
         reward = 0.01
 
     # encourage to speed up in straightaways
-    if abs(steering_angle) < 3 and speed == 2.5:
+    if all_wheels_on_track and abs(steering_angle) < 3 and speed == 2.5:
         reward += speed ** 2 / 4
     else:
         reward += 0.01
