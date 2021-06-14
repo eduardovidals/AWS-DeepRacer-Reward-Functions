5d783ce9986da5b35102e6c7b068eaf6b7005435
Eduardo Vidals <eduardovidals69@gmail.com>
2021-06-14 19:35:40 -0400

    Improved center encouragement reward function

--- RewardFunction.04_CenterImprovement/reward_function.py
+++ RewardFunction.04_CenterImprovement/reward_function.py
@@ -7,23 +7,27 @@ def reward_function(params):
     speed = params['speed']
     progress = params['progress']
     steering_angle = params['steering_angle']
+    is_offtrack = params['is_offtrack']
     reward = 0.001
 
     # encourage for all wheels to be on track
     if all_wheels_on_track:
         # initially encourage to stay in the center
-        if steps < 40:
+        if steps < 10:
             reward = (1 - (distance_from_center / (track_width / 2)) ** 4) * speed ** 2
-        # after 15 steps reward the car based on track completion and speed
-        if steps >= 40:
+        # after 10 steps reward the car based on track completion and speed
+        if steps >= 10:
             reward = ((progress * speed ** 2) / steps) * 2
     else:
-        reward = 0.01
+        reward = 0.001
 
     # encourage to speed up in straightaways
-    if all_wheels_on_track and abs(steering_angle) < 3 and speed == 2.5:
-        reward += speed ** 2 / 4
+    if all_wheels_on_track and abs(steering_angle) < 10 and speed == 2.5:
+        reward += (speed ** 2 / 4) + (progress/steps) ** 2
     else:
-        reward += 0.01
+        reward += 0.001
+
+    if is_offtrack:
+        reward *= 0.5
 
     return float(reward)
