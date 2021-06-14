afebc7b87aff1ed23d993c2a3a3b70308822e3c5
Eduardo Vidals <eduardovidals69@gmail.com>
2021-06-14 19:34:49 -0400

    Self motivator reward function

--- RewardFunction.03_SelfMotivator/reward_function.py
+++ RewardFunction.03_SelfMotivator/reward_function.py
@@ -4,10 +4,10 @@ def reward_function(params):
     """
 
     # read input parameters
-    all_wheels_on_track = params["all_wheels_on_track"]
-    steps = params["steps"]
-    progress = params["progress"]
-    speed = params["speed"]
+    all_wheels_on_track = params['all_wheels_on_track']
+    steps = params['steps']
+    progress = params['progress']
+    speed = params['speed']
 
     if all_wheels_on_track and steps > 0:
         reward = ((progress / steps) * 100) + (speed ** 2)
